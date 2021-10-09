eb83e4f0817876e92b86bd95d00f1b526ac32fed
unknown <fakori.farid@gmail.com>
2021-10-09 19:36:23 -0400

    Assignment1

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+/*Mohammadfarid fakori , 301188346, 2021/10/09*/
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
